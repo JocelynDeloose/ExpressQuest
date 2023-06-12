f350f694f2abb7e39fb7adfbb99fd612b2166811
Jocelyn Deloose <deloosejocelyn@gmail.com>
2023-06-12 09:53:06 +0200

    terminé

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,14 +13,18 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUser);
+app.get("/api/users/:id", userHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
+    const mysql = require("mysql2/promise");
     console.log(`Server is listening on ${port}`);
   }
 });
