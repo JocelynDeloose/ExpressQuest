bf27a69dc7641159847bf8db78915dd9822bc20b
Jocelyn Deloose <deloosejocelyn@gmail.com>
2023-06-27 09:52:54 +0200

    quete 6

--- userHandlers.js
+++ userHandlers.js
@@ -1,17 +1,31 @@
 const database = require("./database");
 
+
 const getUser = (req, res) => {
+  const sql = "select * from users";
+  const sqlValues = [];
+  if (req.query.language != null) {
+    sql += "where language = ?";
+    sqlValues.push(req.query.language);
+  } if (req.query.city != null) {
+    sql += "where city = ?";
+    sqlValues.push(req.query.city);
+  }
+  else if (req.query.language != null) {
+    sql += "where language = ?";
+    sqlValues.push(req.query.language);
+  }
+
   database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users);
+    .query(sql, sqlValues).then(([result]) => {
+      res.json(result);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send("Error retrieving data from database");
+      res.status(500).send("err");
     })
-};
 
+}
 
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
